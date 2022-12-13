d752b57cc1f97f1df77d8f3f4ee8cb219ebf0d88
Nischal Bohara <100176000+nischhal-hub@users.noreply.github.com>
2022-12-13 19:14:24 +0545

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,52 +1,53 @@
-function game(){
-    let playerWin = 0;      //initializing player win score.
-    let computerWin = 0;    //initializing computer win score.
-    for(i=1;i<=5;i++){      // the game will loop for five times.
-        const choices = ["rock","paper","scissor"]; //array for the choices of computer
-        
-        //function that generates random selection 
-        function getComputerSelection(choices){
-            return choices[Math.floor(Math.random()*choices.length)]; //using math.floor to generate lower number than length of array and math.random selects the random number between them.
-        }
-        let computerSelection = getComputerSelection(choices);  
-        
-        let userChoice = prompt('Choose your warrior(Rock/Paper/Scissor):', ''); //prompts the user to select a choice.
-        
-        function userSelection(){           //the selection should be case insensitive so this function lowercases the choice.
-            return userChoice.toLowerCase();
-        }
-        let playerSelection = userSelection(); 
-        function playRound(playerSelection , computerSelection){  //function that counts the win and shows the result of a single session.
-            if((playerSelection == 'rock' && computerSelection == 'scissor') || (playerSelection == 'paper' && computerSelection == 'rock' )||(playerSelection == 'scissor' && computerSelection == 'paper')){
-                playerWin += 1;
-                round = "Player wins!!"+" "+ playerSelection +" "+ "beats" +" "+ computerSelection + "\nPlayer:" + playerWin + "\nComputer:" + computerWin;;
-            }
-            else if((computerSelection == 'rock' && playerSelection == 'scissor')|| (computerSelection == 'paper' && playerSelection == 'rock') ||(computerSelection == 'scissor' && computerSelection == 'paper')){
-                computerWin += 1;
-                round = "Computer wins!!"+" "+ computerSelection +" "+"beats"+" "+playerSelection + "\nPlayer:" + playerWin + "\nComputer:" + computerWin;
-            }
-            else{
-                round = "Tie!! You both choose"+" "+ playerSelection + "\nPlayer:" + playerWin + "\nComputer:" + computerWin;
-            }
-            return round;
-        }
-        console.log(playRound(playerSelection, computerSelection)); //shows result;
-    }
-    function finalResult(playerWin, computerWin){   //function that shows the final result
-        if(playerWin > computerWin){
-            result = "Player wins by" +" "+(playerWin - computerWin);
-        }
-        else if(computerWin > playerWin){
-            result = "Computer wins by" +" "+(computerWin - playerWin);
-        }
+function rps(){
+
+    let c=0;
+    let u=0;
+    while(true){
+        if(u == 5 || c == 5 )
+        break;
         else{
-            result = "It's a tie!! Both scored" +" "+ playerWin;
+            let arr = ["r","p","s"];
+            let randChoice = Math.floor(Math.random()*arr.length);
+            let computerChoice = arr[randChoice];
+            let userChoice = prompt("Select your warrior(r/p/s)");
+            let lowerUserChoice = userChoice.toLowerCase();
+        //console.log(lowerUserChoice);
+            if(lowerUserChoice == "r" && computerChoice == "p"){
+                c++;
+                alert(`Computer wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else if(lowerUserChoice == "p" && computerChoice == "s"){
+                c++;
+                alert(`Computer wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else if(lowerUserChoice == "s" && computerChoice == "r"){
+                c++;
+                alert(`Computer wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else if(lowerUserChoice == "r" && computerChoice == "s"){
+                u++;
+                alert(`User wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else if(lowerUserChoice == "p" && computerChoice == "r"){
+                u++;
+                alert(`User wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else if(lowerUserChoice == "s" && computerChoice == "p"){
+                u++;
+                alert(`User wins \nScores\nComputer:${c}\nUser:${u}`);
+            }
+            else
+                alert(`Draw both selected ${userChoice}`);
         }
-        return result;
+        
     }
-    console.log(finalResult(playerWin, computerWin));
+    alert(`${u<c?"Sorry Computer Wins ":"Congratulations You Won "}The Match!!!!!!!\nFinal Score:\n Computer: ${c}\n User: ${u}`);
 }
-game();
+
+rps();
+
+//console.log(lowerUserChoice);
+//alert(arr[computerChoice]);
 
 
 
